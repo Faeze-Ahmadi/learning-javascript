40f9e9967fced38a1a8b31773383b6887a6fdd87
Faeze <faezeahmadi7173@gmail.com>
2023-07-25 18:11:56 +0330

    faet/faeze/my-own-practice/let-alert

---
+++ js-practice/index.js
@@ -0,0 +1,18 @@
+/*let userName = new String("ali");
+alert(userName); 
+
+let username = new String(`saman`);
+alert(username); */
+
+
+// let name = new String("Faeze");
+// let familyName = new String("ahmadi");
+// let age = parseInt(20);
+
+// alert(typeof name);
+// alert(typeof familyName);
+// alert(typeof age);
+
+
+// let Name = ("Faeze");
+// alert(typeof Name);
