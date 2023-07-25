0e28ab2bbc0b37c3f73bbecd8ab9b7328fd6fb46
Faeze <faezeahmadi7173@gmail.com>
2023-07-25 18:28:45 +0330

    faet/faeze/my-own-practice/js-01

--- js-practice/index.js
+++ js-practice/index.js
@@ -1,21 +1,21 @@
-// let userName = new String("ali");
-// alert(userName); 
+let userName = new String("ali");
+alert(userName); 
 
-// let username = new String(`saman`);
-// alert(username); 
-
-
-// let name = new String("Faeze");
-// let familyName = new String("ahmadi");
-// let age = parseInt(20);
-
-// alert(typeof name);
-// alert(typeof familyName);
-// alert(typeof age);
+let username = new String(`saman`);
+alert(username); 
 
 
-// let Name = ("Faeze");
-// alert(typeof Name);
+let name = new String("Faeze");
+let familyName = new String("ahmadi");
+let age = parseInt(20);
+
+alert(typeof name);
+alert(typeof familyName);
+alert(typeof age);
+
+
+let Name = ("Faeze");
+alert(typeof Name);
 
 
 let seconds = 210;
@@ -24,3 +24,15 @@ let sec = seconds % 60;
 
 alert(min);
 alert(sec);
+
+document.writeln(min, ":", sec);
+
+prompt("enter your age");
+
+let age = +prompt("enter your age:");
+document.writeln(age);
+
+
+let user_dob = 2023 - age;
+
+document.writeln(user_dob);
